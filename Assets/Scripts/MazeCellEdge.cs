be44ba8755dd86bfa4a4b72a3dc7267bd148ed22
Daniel Morgan <me@danielmorgan.co.uk>
2017-08-06 01:21:16 +0100

    Correct opposite door rotation

--- Assets/Scripts/MazeCellEdge.cs
+++ Assets/Scripts/MazeCellEdge.cs
@@ -6,7 +6,7 @@ public abstract class MazeCellEdge : MonoBehaviour {
     public MazeCell parentCell, neighbourCell;
     public MazeDirection direction;
 
-    public void Initialize(MazeCell parentCell, MazeCell neighbourCell, MazeDirection direction) {
+    public virtual void Initialize(MazeCell parentCell, MazeCell neighbourCell, MazeDirection direction) {
         this.parentCell = parentCell;
         this.neighbourCell = neighbourCell;
         this.direction = direction;
--- Assets/Scripts/MazeDoor.cs
+++ Assets/Scripts/MazeDoor.cs
@@ -4,4 +4,18 @@
 public class MazeDoor : MazePassage {
 
     public Transform hinge;
+
+    private MazeDoor OtherSideOfDoor {
+        get { return neighbourCell.GetEdge(direction.GetOpposite()) as MazeDoor; }
+    }
+
+    public override void Initialize(MazeCell parentCell, MazeCell neighbourCell, MazeDirection direction) {
+        base.Initialize(parentCell, neighbourCell, direction);
+        if (OtherSideOfDoor != null) {
+            hinge.localScale = new Vector3(-1f, 1f, 1f);
+            Vector3 p = hinge.localPosition;
+            p.x = -p.x;
+            hinge.localPosition = p;
+        }
+    }
 }
